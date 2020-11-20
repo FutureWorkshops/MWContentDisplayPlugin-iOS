7144930c4c40d0457e1c3d8e5f1db7578e3f39ca
Jonathan Flintham <jonathan@futureworkshops.com>
2020-11-20 12:16:25 +0000

    Improved compositional layout

--- MWVideoGridPlugin/MWVideoGridPlugin/VideoGridStep/Views/MWImageCollectionViewCell.swift
+++ MWVideoGridPlugin/MWVideoGridPlugin/VideoGridStep/Views/MWImageCollectionViewCell.swift
@@ -9,6 +9,8 @@ import Foundation
 import Combine
 import MobileWorkflowCore
 
+protocol MWImageCollectionViewCellDelegate: class {}
+
 class MWImageCollectionViewCell: UICollectionViewCell {
     
     struct ViewData {
@@ -16,6 +18,8 @@ class MWImageCollectionViewCell: UICollectionViewCell {
         let subtitle: String?
         let imageUrl: URL?
     }
+    
+    var delegate: MWImageCollectionViewCellDelegate?
     
     //MARK: - UI
     private let titleLabel: UILabel!
@@ -29,13 +33,15 @@ class MWImageCollectionViewCell: UICollectionViewCell {
         let titleLabel = UILabel()
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.setContentCompressionResistancePriority(.required, for: .vertical)
+        titleLabel.setContentHuggingPriority(.required, for: .vertical)
         titleLabel.font = .preferredFont(forTextStyle: .body, weight: .regular)
         titleLabel.textColor = .label
         self.titleLabel = titleLabel
         
         let subtitleLabel = UILabel()
         subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
-        subtitleLabel.setContentCompressionResistancePriority(.required, for: .vertical)
+        subtitleLabel.setContentCompressionResistancePriority(.required-1, for: .vertical)
+        subtitleLabel.setContentHuggingPriority(.required-1, for: .vertical)
         subtitleLabel.font = .preferredFont(forTextStyle: .caption1, weight: .regular)
         subtitleLabel.textColor = .secondaryLabel
         self.subtitleLabel = subtitleLabel
@@ -46,6 +52,8 @@ class MWImageCollectionViewCell: UICollectionViewCell {
         imageView.layer.cornerRadius = 10.0
         imageView.layer.masksToBounds = true
         self.imageView = imageView
+        
+        self.imageView.heightAnchor.constraint(equalTo: self.imageView.widthAnchor, multiplier: 9/16).isActive = true
         
         let infoStack = UIStackView(arrangedSubviews: [titleLabel, subtitleLabel])
         infoStack.axis = .vertical
@@ -58,12 +66,12 @@ class MWImageCollectionViewCell: UICollectionViewCell {
         let mainStack = UIStackView(arrangedSubviews: [imageView, infoStack])
         mainStack.axis = .vertical
         mainStack.distribution = .fill
-        mainStack.alignment = .leading
+        mainStack.alignment = .fill
         mainStack.spacing = 10
         
         super.init(frame: frame)
         
-        self.contentView.addPinnedSubview(mainStack, order: .top, insets: .zero)
+        self.contentView.addPinnedSubview(mainStack)
         
         self.backgroundColor = .secondarySystemBackground
         self.contentView.backgroundColor = .secondarySystemBackground
@@ -86,7 +94,7 @@ class MWImageCollectionViewCell: UICollectionViewCell {
                 strongSelf.imageView.image = image
             }
         }
-        
+
         self.titleLabel.text = viewData.title
         self.subtitleLabel.text = viewData.subtitle
     }
