9875bf3a2ca7dbf539708446efcc5f9f618b3807
Jonathan Flintham <jonathan@futureworkshops.com>
2021-01-04 15:45:19 +0000

    Adaptation to authentication workflow changes in MWCore

--- MWVideoGrid/MWVideoGrid/AppDelegate.swift
+++ MWVideoGrid/MWVideoGrid/AppDelegate.swift
@@ -8,34 +8,28 @@
 import UIKit
 import MobileWorkflowCore
 
-#if DEBUG
-#else
-func debugPrint(_ items: Any..., separator: String = " ", terminator: String = "\n") {}
-func print(_ items: Any..., separator: String = " ", terminator: String = "\n") {}
-#endif
-
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, AuthRedirector {
-    
-    weak var authFlowResumer: AuthFlowResumer?
-    
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    weak var eventDelegate: AppDelegateEventDelegate?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
     
-    // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        
-        connectingSceneSession.userInfo = [SceneDelegate.SessionUserInfoKey.authRedirectHandler: self.authRedirectHandler()]
-        
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        return self.handleAuthRedirect(for: url)
+        self.eventDelegate?.application(app, open: url, options: options) ?? false
     }
 
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        self.eventDelegate?.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        self.eventDelegate?.application(application, didFailToRegisterForRemoteNotificationsWithError: error)
+    }
 }
