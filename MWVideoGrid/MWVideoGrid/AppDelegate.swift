0c78d6280cb793fd1124abd5d2e5993292487a3f
Jonathan Flintham <jonathan@futureworkshops.com>
2021-03-01 12:45:48 +0000

    Fixed sample project

--- MWVideoGrid/MWVideoGrid/AppDelegate.swift
+++ MWVideoGrid/MWVideoGrid/AppDelegate.swift
@@ -11,7 +11,7 @@ import MobileWorkflowCore
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    weak var eventDelegate: AppDelegateEventDelegate?
+    weak var eventDelegate: AppEventDelegate?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
@@ -19,17 +19,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-    
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        self.eventDelegate?.application(app, open: url, options: options) ?? false
-    }
-
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        self.eventDelegate?.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-    }
-    
-    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        self.eventDelegate?.application(application, didFailToRegisterForRemoteNotificationsWithError: error)
     }
 }
--- MWVideoGrid/MWVideoGrid/SceneDelegate.swift
+++ MWVideoGrid/MWVideoGrid/SceneDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  SceneDelegate.swift
-//  MobileWorkflowCharts
+//  MWVideoGrid
 //
 //  Created by Igor Ferreira on 11/05/2020.
 //  Copyright © 2020 Future Workshops. All rights reserved.
@@ -10,60 +10,22 @@ import UIKit
 import MobileWorkflowCore
 import MWVideoGridPlugin
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    enum SessionUserInfoKey {
-        static let authRedirectHandler = "authRedirectHandler"
+class SceneDelegate: MobileWorkflowSceneDelegate {
+    
+    override func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        self.dependencies.plugins = [
+            MWVideoGridPlugin.self
+        ]
+        
+        super.scene(scene, willConnectTo: session, options: connectionOptions)
     }
     
-    var window: UIWindow?
-    private var urlSchemeManagers: [URLSchemeManager] = []
-    private var rootViewController: MobileWorkflowRootViewController!
-    
-    private var appDelegate: AppDelegate? { UIApplication.shared.delegate as? AppDelegate }
-    
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = scene as? UIWindowScene else { return }
-        
-        let eventService = EventServiceImplementation()
-        self.appDelegate?.eventDelegate = eventService
-        
-        let networkService = NetworkAsyncTaskService()
-        let credentialsStore = CredentialsStore()
-        let authenticationService = AuthenticationService(credentialsStore: credentialsStore, authRedirectHandler: eventService.authRedirectHandler())
-        
-        let manager = AppConfigurationManager(
-            withPlugins: [MWVideoGridPlugin.self],
-            fileManager: .default,
-            credentialsStore: credentialsStore,
-            networkService: networkService,
-            eventService: eventService,
-            asyncServices: [authenticationService]
-        )
-        let preferredConfigurations = self.preferredConfigurations(urlContexts: connectionOptions.urlContexts)
-        self.rootViewController = MobileWorkflowRootViewController(manager: manager, preferredConfigurations: preferredConfigurations)
-        
-        let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = self.rootViewController
-        window.makeKeyAndVisible()
-        self.window = window
-    }
-    
-    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
-        guard let context = self.urlSchemeManagers.firstValidConfiguration(from: URLContexts) else { return }
-        self.rootViewController.loadAppConfiguration(context)
-    }
-}
-
-extension SceneDelegate {
-    
-    private func preferredConfigurations(urlContexts: Set<UIOpenURLContext>) -> [AppConfigurationContext] {
-        
+    override func preferredConfigurations(urlContexts: Set<UIOpenURLContext>) -> [AppConfigurationContext] {
         var preferredConfigurations = [AppConfigurationContext]()
-        
-        if let appPath = Bundle.main.path(forResource: "app", ofType: "json") {
-            preferredConfigurations.append(.file(path: appPath, serverId: 128, workflowId: nil, sessionValues: nil))
+        if let filePath = Bundle.main.path(forResource: "app", ofType: "json") {
+            preferredConfigurations.append(.file(path: filePath, serverId: 128, workflowId: nil, sessionValues: nil))
         }
-        
         return preferredConfigurations
     }
 }
