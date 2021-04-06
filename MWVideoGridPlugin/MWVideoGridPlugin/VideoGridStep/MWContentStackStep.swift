c175a6054f8eb4bbb7b1a240fc9c92e309975c29
Xavi Moll <xmollv@gmail.com>
2021-04-06 12:59:51 +0200

    Added the models and SwiftUI views to render the JSON

--- MWVideoGridPlugin/MWVideoGridPlugin/VideoGridStep/MWContentStackStep.swift
+++ MWVideoGridPlugin/MWVideoGridPlugin/VideoGridStep/MWContentStackStep.swift
@@ -10,7 +10,10 @@ import MobileWorkflowCore
 
 public class MWContentStackStep: ORKStep {
     
-    override init(identifier: String) {
+    let items: [StepItem]
+    
+    init(identifier: String, items: [StepItem]) {
+        self.items = items
         super.init(identifier: identifier)
     }
     
@@ -25,7 +28,99 @@ public class MWContentStackStep: ORKStep {
 
 extension MWContentStackStep: MobileWorkflowStep {
     public static func build(stepInfo: StepInfo, services: MobileWorkflowServices) throws -> Step {
-        return MWContentStackStep(identifier: stepInfo.data.identifier)
+        let jsonItems = (stepInfo.data.content["items"] as? Array<[String:Any]>) ?? []
+        return MWContentStackStep(identifier: stepInfo.data.identifier,
+                                  items: jsonItems.compactMap { StepItem(json: $0) })
     }
 }
+
+// Describes all the supported types of item that can be shown vertically stacked.
+// For now, just title, text and listItem
+// Every case includes the model that defines the concrete implementation as an associated type
+enum StepItem: Identifiable {
+    
+    case title(StepItemTitle)
+    case text(StepItemText)
+    case listItem(StepItemListItem)
+    
+    var id: String {
+        switch self {
+        case .title(let item): return item.id
+        case .text(let item): return item.id
+        case .listItem(let item): return item.id
+        }
+    }
+    
+    init?(json: [String:Any]) {
+        if let stepTypeTitle = StepItemTitle(json: json) {
+            self = .title(stepTypeTitle)
+        } else if let stepTypeText = StepItemText(json: json) {
+            self = .text(stepTypeText)
+        } else if let stepTypeListItem = StepItemListItem(json: json) {
+            self = .listItem(stepTypeListItem)
+        } else {
+            return nil
+        }
+    }
+}
+
+struct StepItemTitle: Identifiable {
+    let id: String
+    let title: String?
+    
+    init?(json: [String:Any]) {
+        guard (json["type"] as? String) == Optional("title") else {
+            return nil
+        }
+        guard let id = json["id"] as? String else {
+            assertionFailure("Missing id.")
+            return nil
+        }
+        self.id = id
+        self.title = json["title"] as? String
+    }
+}
+
+struct StepItemText: Identifiable {
+    let id: String
+    let text: String?
+    
+    init?(json: [String:Any]) {
+        guard (json["type"] as? String) == Optional("text") else {
+            return nil
+        }
+        guard let id = json["id"] as? String else {
+            assertionFailure("Missing id.")
+            return nil
+        }
+        self.id = id
+        self.text = json["text"] as? String
+    }
+}
+
+struct StepItemListItem: Identifiable {
+    let id: String
+    let title: String?
+    let detailText: String?
+    let imageURL: URL?
+    
+    init?(json: [String:Any]) {
+        guard (json["type"] as? String) == Optional("listItem") else {
+            return nil
+        }
+        guard let id = json["id"] as? String else {
+            assertionFailure("Missing id.")
+            return nil
+        }
+        self.id = id
+        self.title = json["text"] as? String
+        self.detailText = json["detailText"] as? String
+        if let imageURLString = json["imageURL"] as? String {
+            self.imageURL = URL(string: imageURLString)
+        } else {
+            self.imageURL = nil
+        }
+    }
+}
+
 
--- MWVideoGridPlugin/MWVideoGridPlugin/VideoGridStep/MWContentStackViewController.swift
+++ MWVideoGridPlugin/MWVideoGridPlugin/VideoGridStep/MWContentStackViewController.swift
@@ -15,16 +15,68 @@ final class MWContentStackViewController: ORKStepViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.addCovering(childViewController: UIHostingController(rootView: ContentView(step: self.contentStackStep)))
+        self.addCovering(childViewController: UIHostingController(rootView: MWContentView(step: self.contentStackStep)))
     }
     
 }
 
-struct ContentView: View {
+private struct MWContentView: View {
     
     @State var step: MWContentStackStep
     
     var body: some View {
-        Text(step.title ?? "No Title")
+        ScrollView {
+            VStack(alignment: .leading, spacing: 8) {
+                ForEach(self.step.items) { item in
+                    switch item {
+                    case .title(let innerItem): MWTitleView(stepTypeTitle: innerItem)
+                    case .text(let innerItem): MWTextView(stepTypeText: innerItem)
+                    case .listItem(let innerItem): MWListItemView(stepTypeListItem: innerItem)
+                    }
+                }
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .leading)
+            .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
+        }
+    }
+}
+
+private struct MWTitleView: View {
+    
+    let stepTypeTitle: StepItemTitle
+        
+    var body: some View {
+        Text(stepTypeTitle.title ?? "MISSING_TITLE")
+            .font(.largeTitle)
+    }
+}
+
+private struct MWTextView: View {
+    
+    let stepTypeText: StepItemText
+        
+    var body: some View {
+        Text(stepTypeText.text ?? "MISSING_TEXT")
+    }
+}
+
+private struct MWListItemView: View {
+    
+    let stepTypeListItem: StepItemListItem
+    
+    var body: some View {
+        HStack {
+            #warning("Ignoring the image until we can load it correctly")
+            if let _ = stepTypeListItem.imageURL {
+                Image(systemName: "opticaldisc")
+                    .resizable()
+                    .frame(width: 44, height: 44, alignment: .center)
+            }
+            VStack(alignment: .leading) {
+                Text(stepTypeListItem.title ?? "MISSING_TITLE")
+                    .fontWeight(.semibold)
+                Text(stepTypeListItem.detailText ?? "MISSING_DETAIL_TEXT")
+            }
+        }
     }
 }
