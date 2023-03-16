85173d83372594b2612616354188d2b643c3110b
Igor Ferreira <me@igorcferreira.dev>
2023-03-16 18:50:27 +0100

    Add metadata to Network Grid step

--- MWContentDisplayPlugin/MWContentDisplayPlugin/ContentGridStep/Network/MWNetworkGridStep.swift
+++ MWContentDisplayPlugin/MWContentDisplayPlugin/ContentGridStep/Network/MWNetworkGridStep.swift
@@ -73,3 +73,52 @@ extension MWNetworkGridStep: BuildableStep {
         return MWNetworkGridStep(identifier: stepInfo.data.identifier, stepInfo: stepInfo, services: services, url: remoteURLString, emptyText: emptyText)
     }
 }
+
+public class GridNetworkGridMetadata: StepMetadata {
+    enum CodingKeys: String, CodingKey {
+        case url
+        case emptyText
+        case navigationItems = "_navigationItems"
+    }
+    
+    let url: String
+    let emptyText: String?
+    let navigationItems: [NavigationItemMetadata]?
+    
+    init(id: String, title: String, url: String, emptyText: String?, navigationItems: [NavigationItemMetadata]?, next: PushLinkMetadata?, links: [LinkMetadata]) {
+        self.url = url
+        self.emptyText = emptyText
+        self.navigationItems = navigationItems
+        super.init(id: id, type: "networkVideoGrid", title: title, next: next, links: links)
+    }
+    
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.url = try container.decode(String.self, forKey: .url)
+        self.emptyText = try container.decodeIfPresent(String.self, forKey: .emptyText)
+        self.navigationItems = try container.decodeIfPresent([NavigationItemMetadata].self, forKey: .navigationItems)
+        try super.init(from: decoder)
+    }
+    
+    public override func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.url, forKey: .url)
+        try container.encodeIfPresent(self.emptyText, forKey: .emptyText)
+        try container.encodeIfPresent(self.navigationItems, forKey: .navigationItems)
+        try super.encode(to: encoder)
+    }
+}
+
+public extension StepMetadata {
+    static func gridNetworkGrid(
+        id: String,
+        title: String,
+        url: String,
+        emptyText: String? = nil,
+        navigationItems: [NavigationItemMetadata]? = nil,
+        next: PushLinkMetadata? = nil,
+        links: [LinkMetadata] = []
+    ) -> GridNetworkGridMetadata {
+        GridNetworkGridMetadata(id: id, title: title, url: url, emptyText: emptyText, navigationItems: navigationItems, next: next, links: links)
+    }
+}
